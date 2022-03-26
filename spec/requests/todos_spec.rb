295b058b877cb8bb459396ec6764bd059e781322
cantstopcoding <jonathan_koss@yahoo.com>
2022-03-26 11:32:27 -0400

    check to see if the response object has a successful HTTP status range (200 to 299)

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 describe 'Todos API', type: :request do
   it 'returns all todos' do
     get 'http://localhost:3000/api/v1/todos'
+
+    expect(response).to have_http_status(:success)
   end
 end
